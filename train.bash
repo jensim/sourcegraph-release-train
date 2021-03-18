485ff186baa962b9c649386c6e03973b8e0bab32
Jens Brimfors <jens.brimfors@nordnet.se>
2021-03-18 15:56:01 +0100

    Created a naive release train script

---
+++ train.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -xe
+
+cd "$(git rev-parse --show-toplevel)"
+
+source '.env'
+
+[ -n "$tag" ]
+[ -n "$docker_hub_user" ]
+[ -n "$docker_hub_repo" ]
+
+export IMAGE="$docker_hub_user/$docker_hub_repo"
+export VERSION="$TAG-oss"
+
+rm -rf sourcegraph
+git clone git@github.com:sourcegraph/sourcegraph.git
+cd sourcegraph
+
+git fetch --all --tags
+git checkout "tags/$tag" -b "$tag-release-branch-$docker_hub_user"
+
+cd cmd/server
+./pre-build.sh
+./build.sh
+
+
+echo '===========
+=  Done!  =
+==========='
